d6ee11e6250f93996e230e2b58622ee044562294
Thiago <thiago.usetelni@visionnaire.com.br>
2021-12-14 14:15:53 -0300

    Project wireframe.

---
+++ src/main/java/br/com/usetelni/jayaexchange/JayaExchangeApplication.java
@@ -0,0 +1,13 @@
+package br.com.usetelni.jayaexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayaExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JayaExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/usetelni/jayaexchange/JayaExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usetelni.jayaexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayaExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
